31b88c8048ac11429823ab3c420b4ee67fd0fb7a
braverokmc79 <braverokmc79@gmail.com>
2025-01-20 21:41:17 +0900

    8.Sidebar Header 구성 및 Theme Toggle 추가

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ThemeToggle:React.FC = () => {
+  return (
+    <button className='btn btn-primary btn-sm'>
+        ThemeToggle
+    </button>
+  )
+}
+
+export default ThemeToggle
